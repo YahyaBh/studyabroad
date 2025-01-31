f852039213649203b8d2b8c1684a462f4734edff
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-01-31 23:56:43 +0100

    Footer last upgrade

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import { FaArrowDown, FaLocationArrow, FaSchool, FaSearch, FaStar } from "react-icons/fa";
+import { FaArrowDown, FaLocationArrow, FaPhone, FaSchool, FaSearch, FaStar } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
-import { MdArrowOutward } from "react-icons/md";
+import { MdArrowOutward, MdEmail } from "react-icons/md";
 import "./page.scss";
 
 export default function Home() {
@@ -556,6 +556,55 @@ export default function Home() {
 
 
       <footer>
+
+
+        <div className="footer-container">
+
+
+
+          <div className="cont">
+            <ul>
+              <li><FaSchool/> Satyamstudio</li>
+              <li><FaLocationArrow/> 8819 Ohio St. South Gate, CA 90280</li>
+              <li><MdEmail/> Ourstudio@hello.com</li>
+              <li><FaPhone/> +1 386-688-3295</li>
+            </ul>
+          </div>
+
+          <div className="cont">
+            <h4>Services</h4>
+
+            <ul>
+              <li>Travel Organization </li>
+              <li>Preparing the Visa file</li>
+              <li>University Admission</li>
+              <li>University Scholarship</li>
+              <li>Education Consultation</li>
+            </ul>
+          </div>
+
+          <div className="cont">
+            <h4>Menu</h4>
+
+            <ul>
+              <li>About Us</li>
+              <li>Countries</li>
+              <li>Universities  </li>
+            </ul>
+
+          </div>
+
+
+          <div className="cont">
+            <h4>Join a Newsletter</h4>
+
+            <p>Subscribe to our newsletter to get the latest news and updates.</p>
+
+            <input type="email" placeholder="Enter your email" />
+
+            <button>Subscribe</button>
+          </div>
+        </div>
 
       </footer>
 
