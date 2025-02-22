61fb64d88f1b1b18be981e475b721bdbb31e8be6
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-02-22 17:42:43 +0100

    Last Commit

--- src/app/api/verify-email/route.js
+++ src/app/api/verify-email/route.js
@@ -1,77 +1,42 @@
+import { setCookie } from 'cookies-next';
+import { client } from '../../lib/sanityClient';
+import jwt from 'jsonwebtoken';
 
 export async function GET(req) {
-
-    let tokenDate = '';
-
-
     const { searchParams } = new URL(req.url);
     const token = searchParams.get('token');
 
-
-    fetchUserByToken = async (token) => {
-        try {
-            const query = `*[_type == "user" && token == $token][0]`;
-            const params = { token };
-
-            const user = await client.fetch(query, params);
-
-            if (user) {
-                tokenDate = user.token;
-            } else {
-                console.log('No user found with this token');
-                return null;
-            }
-        } catch (error) {
-            console.error('Error fetching user by token:', error);
-            return null;
-        }
-    };
-
-
-
-    if (tokenDate === '') {
-        return new Response(
-            JSON.stringify({ error: 'Verification token or email not found in cookies' }),
-            { status: 400 }
-        );
+    if (!token) {
+        return new Response(JSON.stringify({ error: 'Unable to retrieve authorization token' }), { status: 400 });
     }
 
-    if (token === tokenDate) {
-        try {
-            // Find the user with the matching token
-            const user = await client.fetch(
-                `*[_type == "user" && token == $token][0]`,
-                { token }
-            );
+    try {
+        console.log('Received token:', token);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log('Decoded token:', decoded);
 
-            if (!user) {
-                throw new Error('Invalid token');
-            }
 
-            // Set the user as verified
-            await client.patch(user._id)
-                .set({ verified: true, token: null }) // Remove token after verification
-                .commit();
+        const user = await client.fetch(`*[_type == "user" && token == $token][0]`, {
+            token: token,
+        });
 
-            console.log('User verified successfully');
-
-            return new Response(
-                JSON.stringify({ message: 'Email verified successfully!' }),
-                { status: 200 }
-            );
-        } catch (error) {
-            console.error('Error verifying user:', error);
+        if (!user) {
+            return new Response(JSON.stringify({ error: 'User not found' }), { status: 404 });
         }
 
+        setCookie('token', token);
 
-    } else {
-        return new Response(
-            JSON.stringify({ error: 'Invalid verification token or email' }),
-            { status: 401 } // Unauthorized status
-        );
+        console.log(user);
+
+
+        await client
+            .patch(user._id)
+            .set({ verified: true, token: '' })
+            .commit();
+
+        // Redirect the user to the verification success page
+        return new Response(null, { status: 302, headers: { Location: `/consultation/verified/${user._id}` } });
+    } catch (err) {
+        return new Response(JSON.stringify({ error: 'Invalid or expired token' }), { status: 400 });
     }
 }
-
-export const config = {
-    matcher: ['/api/send-verification'],
-};
